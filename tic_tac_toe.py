427771d5c9b54639f76764cbaf765710f9cfd0a0
Antoni Solarski <antsol1000@gmail.com>
2020-07-29 12:35:58 +0200

    changed size of buttons

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -17,7 +17,7 @@ def click(button_ptr):
     ptr += 1
 
     buttons.pop(button_ptr)
-    buttons.insert(button_ptr, Button(root, width=20, height=10, text=char, state=DISABLED))
+    buttons.insert(button_ptr, Button(root, width=16, height=8, text=char, state=DISABLED))
 
     board.pop(button_ptr)
     board.insert(button_ptr, char)
@@ -31,7 +31,7 @@ def click(button_ptr):
         for j in range(9):
             if board[j] == "":
                 buttons.pop(j)
-                buttons.insert(j, Button(root, width=20, height=10, state=DISABLED))
+                buttons.insert(j, Button(root, width=16, height=8, state=DISABLED))
         for j in range(9):
             buttons[j].grid(row=j // 3, column=j % 3)
 
@@ -41,15 +41,15 @@ def click(button_ptr):
             Label(root, text=position + " won this time!", width=15, height=5).grid(row=3, column=1)
 
 
-buttons = [Button(root, width=20, height=10, command=lambda: click(0)),
-           Button(root, width=20, height=10, command=lambda: click(1)),
-           Button(root, width=20, height=10, command=lambda: click(2)),
-           Button(root, width=20, height=10, command=lambda: click(3)),
-           Button(root, width=20, height=10, command=lambda: click(4)),
-           Button(root, width=20, height=10, command=lambda: click(5)),
-           Button(root, width=20, height=10, command=lambda: click(6)),
-           Button(root, width=20, height=10, command=lambda: click(7)),
-           Button(root, width=20, height=10, command=lambda: click(8))]
+buttons = [Button(root, width=16, height=8, command=lambda: click(0)),
+           Button(root, width=16, height=8, command=lambda: click(1)),
+           Button(root, width=16, height=8, command=lambda: click(2)),
+           Button(root, width=16, height=8, command=lambda: click(3)),
+           Button(root, width=16, height=8, command=lambda: click(4)),
+           Button(root, width=16, height=8, command=lambda: click(5)),
+           Button(root, width=16, height=8, command=lambda: click(6)),
+           Button(root, width=16, height=8, command=lambda: click(7)),
+           Button(root, width=16, height=8, command=lambda: click(8))]
 
 for i in range(9):
     buttons[i].grid(row=i // 3, column=i % 3)
