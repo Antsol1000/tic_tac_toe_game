6acf6a8d5f9b215d2884b4412860f258a20f7a9a
Antoni Solarski <antsol1000@gmail.com>
2020-07-29 12:31:09 +0200

    divided code into two files, moved status checking function to another file

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import status
 
 root = Tk()
 root.title("TIC TAC TOE GAME")
@@ -7,27 +8,6 @@ sign = ['X', 'O']
 ptr = 0
 
 board = [""] * 9
-
-
-def check(table):
-
-    for j in range(3):
-        # check for win in rows
-        if table[3 * j] == table[3 * j + 1] == table[3 * j + 2] and table[3 * j] != "":
-            return table[3 * j]
-        # check for win in columns
-        if table[j] == table[j + 3] == table[j + 6] and table[j] != "":
-            return table[j]
-
-    # check for win in diagonals
-    if table[0] == table[4] == table[8] and table[4] != "":
-        return table[4]
-    if table[2] == table[4] == table[6] and table[4] != "":
-        return table[4]
-
-    if "" not in table:
-        return "draw"
-    return "go"
 
 
 def click(button_ptr):
@@ -45,7 +25,7 @@ def click(button_ptr):
     for j in range(9):
         buttons[j].grid(row=j // 3, column=j % 3)
 
-    position = check(board)
+    position = status.check(board)
     if position != "go":
 
         for j in range(9):
